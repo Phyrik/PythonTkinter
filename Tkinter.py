87b6d0c14c5f1545e1b66283985307b484331e82
Phyrik <34688504+Phyrik@users.noreply.github.com>
2018-07-07 16:43:08 +0100

    Added menus: file and edit

--- Tkinter.py
+++ Tkinter.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 
+def doNothing():
+    print("Doing nothing...")
+
 root = tk.Tk()
 
 menu = tk.Menu(root)
@@ -7,6 +10,14 @@ root.config(menu=menu)
 
 subMenu = tk.Menu(menu)
 menu.add_cascade(label="File", menu=subMenu)
-subMenu.add_command(label)
+subMenu.add_command(labe="New Project...", command=doNothing)
+subMenu.add_command(labe="New", command=doNothing)
+
+subMenu.add_separator()
+subMenu.add_command(label="Exit", command=doNothing)
+
+editMenu = tk.Menu(menu)
+menu.add_cascade(label="Edit", menu=editMenu)
+editMenu.add_command(label="Redo", command=doNothing)
 
 root.mainloop()
